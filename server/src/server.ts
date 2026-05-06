5cb841601bc8f25224913ef9c7f6762976c13313
Russell Ferrero <russellferrero00@gmail.com>
2026-05-06 16:36:36 +0800

    fix: load environment before server imports

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,6 @@
+import 'dotenv/config';
 import app from './app';
-import dotenv from 'dotenv';
 import { connectDB } from './config/db';
-
-dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
